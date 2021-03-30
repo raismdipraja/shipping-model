6f33588197ff4583066f4e7ce02538d1028ef001
Rais Dipraja <diprajarr@gmail.com>
2021-03-30 02:36:10 -0700

    update api

--- server/controllers/orderFilter.controller.js
+++ server/controllers/orderFilter.controller.js
@@ -1,8 +1,7 @@
 import {sequelize} from '../models/IndexModel'
 
 const orderFilter = async (req, res)=>{
-    const filterOrder = await sequelize.query(`select order_name, order_created_on, order_subtotal, order_weight, order_discount, order_stat_name
-    from orders
+    const filterOrder = await sequelize.query(`select * from orders join account on account.acco_id = order_acco_id
     where order_acco_id_seller=:id_seller and order_stat_name=upper(:status)`,{
         replacements: {
             id_seller: req.params.id_seller,
